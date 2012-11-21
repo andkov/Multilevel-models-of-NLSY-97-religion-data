ef1e884b71070932f5bcb3cb8e2a5aee59f80baf
Andrey Koval <koval.andrey@gmail.com>
2012-11-20 20:23:33 -0600

    d

--- ISE_NLSY97_Religion_age2000/NLSY97_creating_the_plot.R
+++ ISE_NLSY97_Religion_age2000/NLSY97_creating_the_plot.R
@@ -4,6 +4,8 @@ require(colorspace) #Load the library necessary for creating tightly-controlled 
 pathInput <- file.path(getwd(), "LmerExample", "Day2MlmExampleData.csv") #Location of the data file.
 
 ds <- dssmall
+ds$byearci<-ds$byearc+1
+
 # ds$GroupF <- factor(ds$GroupID) #Create a 'factor' for Group (which is necessary for the multilevel model)
 # ds$SubjectF <- factor(ds$SubjectID)#Create a 'factor' for Subject (which is necessary for the multilevel model)
 
@@ -22,12 +24,12 @@ names(co) #Inspect the names of the four coefficients.
 #For color information see http://cran.r-project.org/web/packages/colorspace/vignettes/hcl-colors.pdf
 # and http://statmath.wu.ac.at/~zeileis/papers/Zeileis+Hornik+Murrell-2009.pdf
 str(ds)
-ds$byearci<-ds$byearc+1 # for 
+
 pchGroup <- 1 # sort(unique(ds$byearc)) #Define the shapes used for each group. In this case, it's just a 1 and 2.
 colorSubject <- rainbow_hcl(n=length(unique(ds$id))) #Define colors for each subject
-colorGroup <- rainbow_hcl(n=length(unique(ds$byearci))) #Define colors for each group
-colorGroupAlpha <- adjustcolor(colorGroup, alpha.f=.5) #Define translucent colors for each group
-colorGroupDark <- rainbow_hcl(n=length(unique(ds$byearci)), c=50, l=60) #Define slightly darker colors for each group (usd for their mean slope).
+colorGroup <- rainbow_hcl(n=length(unique(ds$byearc))) #Define colors for each group
+colorGroupAlpha <- adjustcolor(colorGroup, alpha.f=.02) #Define translucent colors for each group
+colorGroupDark <- rainbow_hcl(n=length(unique(ds$byearc)), c=50, l=60) #Define slightly darker colors for each group (usd for their mean slope).
 
 xRange <- range(ds$time, na.rm=TRUE) #To be used for the x-axis limits (and remove and missing/NA values).
 yRange <- range(ds$attend, na.rm=TRUE) #To be used for the x-axis limits
@@ -76,16 +78,31 @@ for( groupID in unique(ds$byearci) ) { #Iterate through each group in the datase
 }
 rm(dsSlice) # Removes the variable from memory
 
+# cohort<-1981
+# ds<-dsattendlong [which(dsattendlong$byear==cohort & dsattendlong$id>1),]
+# ds<-dsattendlong [which(dsattendlong$byear==cohort),]
+# ds$byearci<-ds$byearc+1 # 
+
 #This loop iterates over each subject and 
+# title(cohort)
 for( i in unique(ds$id) ) { 
   dsSlice <- subset(ds, id==i) #Select only that subject's data
-  xs <- dsSlice$time #Should we jitter?
+  xs <- jitter(dsSlice$time,1) #Should we jitter?
   ys <- dsSlice$attend
   #lines(x=xs, y=ys)
-  lines(x=xs, y=ys, col=colorGroup[dsSlice$byearci]  )
-  #lines(x=xs, y=ys, col=colorGroupAlpha[dsSlice$byearci]) #Draw their line, with their group's color
+  lines(x=xs, y=ys, col=colorGroup[dsSlice$byearci])
+  #lines(x=xs, y=ys, col=colorGroupAlpha[dsSlice$byearc]) #Draw their line, with their group's color
 }
 rm(dsSlice) #Remove the variable from memory
+
+#?curve
+c<-c(2000, 2010)
+x<-2000:2010
+a<-2
+b<-.1
+y<-a+b*x
+plot.window(xlim=c(2000 2010), ylim=c(1 8))
+curve(a+b*x, from=0, to=10,add=F)
 
 summary(m6s)
 fixef(m6s) # defines the line for reference - given all predictors are 0
@@ -96,12 +113,12 @@ names(co) #Inspect the names of the four coefficients.
 print (co)
 
 #Plot the summary for Group1 (which is just the intercept plus the slope*time)
-curve(co["(Intercept)"]  + co["timec"]*x , add=T, col=colorGroupDark[1], lwd=4)
+curve(co["(Intercept)"]  + co["timec"]*x , add=T, col="red", lwd=4)
 #Plot the summary for Group2 (which now includes the Group 2 offsets for intercept & slope)
 curve(co["(Intercept)"] + co["byearc"]*x + co["timec"]*x + co["byearc:timec"]*x, add=T, col=colorGroupDark[2], lwd=4)
 
 #Annotate the clinical threshold of 13.
-threshold <- 3.44
+threshold <- 2.38345
 abline(h=threshold, col="blue")
 text(x=1, y=threshold, labels="Clinical\nThreshold", pos=4, col="blue")
 
